a19c0da37508bce77c7747af86747ca93ef2fa28
d15125895 <33666054+d15125895@users.noreply.github.com>
2018-02-14 16:27:48 +0000

    create a private fields

--- Tune.java
+++ Tune.java
@@ -2,6 +2,16 @@ package ie.dit;
 
 public class Tune // must be same as file name
 {
+	
+	
+	private int x;
+	private char title;
+	private char altTitle ;
+	private char notation;
+	
+	
+	
+	
 	public static void main (String[] args) // main methode
 	{
 		
